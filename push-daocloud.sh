d6964dd3ed80bfd19c27c941a70b48534377b144
jiajia.tang <jiajia.tang@baidao.com>
2018-01-26 16:16:45 +0800

    modify .sh

--- push-daocloud.sh
+++ push-daocloud.sh
@@ -4,17 +4,17 @@ tag=$(date +%Y%m%d%H%M%S)
 
 imageName=gmg-adviser-qrload-service
 
-dcloudImage="registry.cn-shanghai.aliyuncs.com/ytx-zq/${imageName}:${tag}"
-dcloudImage1="registry.cn-shanghai.aliyuncs.com/ytx-zq/${imageName}:latest"
+dcloudImage="${imageName}:${tag}"
+dcloudImage1="${imageName}:latest"
 
 
 docker build -t ${imageName} .
 
-docker tag -f ${imageName} ${dcloudImage}
-docker push ${dcloudImage}
+docker tag -f "${imageName} ${dcloudImage}"
+docker push "${dcloudImage}"
 
-docker tag -f ${imageName} ${dcloudImage1}
-docker push ${dcloudImage1}
+docker tag -f "${imageName} ${dcloudImage1}"
+docker push "${dcloudImage1}"
 
 echo '============ push success ============'
 
